e4a9ad1fafc04abb0220ac5465056a82bcc52048
wlsp <wlsp@outlook.de>
2021-05-06 16:38:26 +0200

    how-old

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+
+
+function changeLetterSpacing() {
+    // res.style.letterSpacing = "10px"
+    res.style.animationPlayState = "running"
+}
+
+// window.alert('Hallo Welt')
+let input = window.prompt('Wie alt bist Du)')
+console.log(input)
+
+console.log("Wow du bist " + input + "Jahre alt!..")
+alert(("Fuck Negro, ") + input + "???!" + " Bist du Scheißealt")
+
+const res = document.getElementById('res')
+res.innerHTML = (input + "!" + " Fuck Negro you OLD my G!")
+res.style.fontSize = "55px"
+
